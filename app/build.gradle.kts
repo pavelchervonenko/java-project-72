7079d4c4047e4673824a64955df4cb5483ee1a3a
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-10-26 22:30:34 +0300

    initialization of the project

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("java")
+    id("checkstyle")
+    id("jacoco")
+    id("org.sonarqube") version "7.0.1.6134"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("${rootDir}/config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "pavelchervonenko_java-project-72")
+        property("sonar.organization", "pavelchervonenko")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml").get().asFile.absolutePath
+        )
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
