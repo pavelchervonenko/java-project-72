aa9c5cb0d153f152205b336cb3999fbc65b72067
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-10-29 23:05:30 +0300

    add model for url

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,14 +1,17 @@
 package hexlet.code.model;
 
+import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class Url {
     private Long id;
     private String name;
