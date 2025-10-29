ffdec82c00f6ec32000d17157f97b91de99da993
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-10-29 23:04:35 +0300

    add NamedRoutes

---
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -0,0 +1,20 @@
+package hexlet.code.util;
+
+public class NamedRoutes {
+
+    public static String rootPath() {
+        return "/";
+    }
+
+    public static String urlsPath() {
+        return "/urls";
+    }
+
+    public static String urlPath() {
+        return "/urls/{id}";
+    }
+
+    public static String urlPath(Long id) {
+        return "/urls/" + id;
+    }
+}
